c17bbe67ee981505e03d757078f4242ebafeb610
Anusha-296 <121625693+Anusha-296@users.noreply.github.com>
2024-05-27 22:45:58 +0530

    doing cicd project

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('You are  in AWS  Cloud. doing project on jenkins cicd pipleline , webhook'); //write a response to the client
+  res.write('You are  in AWS  Cloud. i am doing project on jenkins cicd pipleline , webhook'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
